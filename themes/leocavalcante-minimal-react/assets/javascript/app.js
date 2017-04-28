a959d4a2972be458f8abc7bb5585e505a45b33b3
santeeno123 <santeenoalex@gmail.com>
2017-04-28 16:48:57 +0300

    1.0.3

---
+++ themes/leocavalcante-minimal-react/assets/javascript/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (<main><header><h1>HELLO WORLD!</h1></header></main>);
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
